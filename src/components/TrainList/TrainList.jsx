1daf99d3f4c119594a15f1ed404c00b379076914
dubikdenis07/-bc2025-1 <dubikddenis07@gmail.com>
2026-05-14 18:34:42 +0200

    Створення списку потягів

--- src/components/TrainList/TrainList.jsx
+++ src/components/TrainList/TrainList.jsx
@@ -1,5 +1,22 @@
-function TrainList() {
-    return <div>Train List</div>;
+import TrainCard from "../TrainCard/TrainCard";
+import "./TrainList.css";
+
+function TrainList({ trains }) {
+    if (trains.length === 0) {
+        return (
+            <div className="train-list__empty">
+                <h2>Рейсів не знайдено</h2>
+                <p>Спробуйте змінити номер потяга або назву міста у пошуку.</p>
+            </div>
+        );
+    }
+    return (
+        <section className="train-list">
+            {trains.map((train) => (
+                <TrainCard key={train.id} train={train} />
+            ))}
+        </section>
+    );
 }
 
 export default TrainList;
